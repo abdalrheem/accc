65ff73354e6bd30000193933b173fb73a20d187d
Abdalrheem <abdalrheem.a1995@gmail.com>
2023-11-10 20:22:08 +0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import NavBar from "./components/Navbar";
+import { BrowserRouter as Route, Router } from "react-router-dom";
+import Services from "./components/ProjectsSection";
+import Home from "./components/Home";
+import ColorModeSwitch from "./components/ColorModeSwitch";
+
+const App = () => {
+  return (
+    <>
+      <Home />
+      <ColorModeSwitch />
+    </>
+  );
+};
+
+export default App;
